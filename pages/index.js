41fa600baadcfea24dd2a8607f3281e6cb5e0dcd
julian <julian.costinea@gmail.com>
2022-08-03 17:36:33 +0200

    added z index to navbar

--- pages/index.js
+++ pages/index.js
@@ -47,10 +47,20 @@ export default function Privat() {
                 </div>
             </div>
             <div className={classes.Details}>
-                <h2>
-                    Private details
-                </h2>
-                <p>Cillum minim in fugiat sit nostrud excepteur mollit ad consectetur elit et irure aliqua. Eu incididunt officia nostrud tempor consequat nisi. Cillum minim commodo excepteur proident amet ullamco voluptate duis consequat ea aliqua culpa. Labore occaecat sint laborum exercitation quis non aliqua.</p>
+                <h1>
+                    Servicii ecologice si <span>fara</span> perioada minima contractuala
+                </h1>
+                <p>Sunteti pe maini complet sigure daca alegeti CompletCurat pentru a va mentine casa curata si prezentabila.
+                    Avem o vasta experienta cu ajutorul curateniei in case private si avem in prezent peste 1.000 de clienti fideli.
+                    Stim ca mutarea in casele private este o munca foarte responsabila, motiv pentru care nu facem niciodata compromisuri cu privire la valorile noastre fundamentale.
+                    Va tratam bunurile ca si cum ar fi ale noastre si puteti fi siguri ca suntem intotdeauna atenti cand ocolim toate colturile si colturile. Suntem ajutorul tau casnic.
+                </p>
+                <p>
+                    Te vei intalni cu echipa permanenta la prima curatenie privata, unde vei parcurge impreuna casa si programul de curatenie agreat.
+                    Amintiti-va ca suntem flexibili cu boala, vacante etc. si, prin urmare, puteti anula sau reprograma serviciul de curatenie in orice moment,
+                    in conformitate cu politica noastra de anulare.
+                    Recomandam clientilor nostri sa achizitioneze o curatare amanuntita de inceput, in care ne asiguram ca ajungem la capatul sarcinii,
+                    astfel incat exista un bun punct de plecare de la care sa incepem.</p>
             </div>
             <div className={classes.Details}>
                 <h2>
