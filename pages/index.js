da22faae538070781bb62308305ec3ccfd19c059
julian <julian.costinea@gmail.com>
2022-08-08 16:52:18 +0200

    finished homepage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { PrivatPhoto } from "../components/UI/PrivatPhoto/PrivatPhoto";
 import { FirmaPhoto } from "../components/UI/FirmaPhoto/FirmaPhoto";
 import { HomeIcon, BuildingIcon } from "../components/UI/Icons/Icons";
 import Link from "next/link";
+import ContactFrom from "../components/ContactForm/ContactForm";
 
 import classes from "../styles/Home.module.css";
 
@@ -45,72 +46,48 @@ export default function Home() {
         </div>
       </div>
       <div className={classes.Details}>
-        {/* START HERE */}
         <h1>
-          Curatenie privata pentru <span>nevoile si dorintele</span>{" "}
-          dumneavoastra
+          PERSONALIZAM O SOLUTIE <span>PROFESIONALA</span> PENTRU DVS. SAU COMPANIA DVS
         </h1>
         <p>
-          Sunteti pe maini complet sigure daca alegeti CompletCurat pentru a va
-          mentine casa curata si prezentabila. Avem o vasta experienta cu
-          ajutorul curateniei in case private si avem in prezent peste 1.000 de
-          clienti fideli. Stim ca mutarea in casele private este o munca foarte
-          responsabila, motiv pentru care nu facem niciodata compromisuri cu
-          privire la valorile noastre fundamentale. Va tratam bunurile ca si cum
-          ar fi ale noastre si puteti fi siguri ca suntem intotdeauna atenti
-          cand ocolim toate colturile si colturile. Suntem ajutorul tau casnic.
+          Dupa o serie de intrebari, am creat un pachet cuprinzator de asistenta profesionala pentru curatenie, care include atat curatenie privata,
+          cat si curatenie comerciala. Avem mai multi clienti comerciali carora le facem atat curat in birouri, dar ne ocupam si de curatarea locuintelor tuturor angajatilor.
         </p>
         <p>
-          Te vei intalni cu echipa permanenta la prima curatenie privata, unde
-          vei parcurge impreuna casa si programul de curatenie agreat.
-          Amintiti-va ca suntem flexibili cu boala, vacante etc. si, prin
-          urmare, puteti anula sau reprograma serviciul de curatenie in orice
-          moment, in conformitate cu politica noastra de anulare. Recomandam
-          clientilor nostri sa achizitioneze o curatare amanuntita de inceput,
-          in care ne asiguram ca ajungem la capatul sarcinii, astfel incat
-          exista un bun punct de plecare de la care sa incepem.
+          Usuram treburile zilnice, indiferent de marime, cu un zambet, pentru ca tu sa ai timp si energie in viata de zi cu zi sau in afacerea ta.
+          Daca vorbim de sarcini speciale, o vedem doar ca pe o provocare interesanta si educationala,
+          asa ca va rugam sa nu ezitati sa contactati serviciul nostru pentru clienti daca aveti intrebari.
         </p>
       </div>
       <div className={classes.SatisfiedCustomers}>
-        <div className={classes.SatisfiedCustomersData}>
-          <h1>+700</h1>
-          <h1>Clienti privati si comerciali</h1>
+        <h1>3 MOTIVE BUNE PENTRU A ALEGE C.C. CURATARE</h1>
+        <div className={classes.infoContainer}>
+          <div className={classes.infoDiv}>
+            <h3>ANGAJATI FERICITI</h3>
+            <p>Angajatul nostru este resursa noastra cea mai importanta pentru a obtine cele mai bune rezultate. De aceea acordam o mare importanta angajatilor fericiti!
+              Ne asiguram intotdeauna sa fim inovatori si sa ne imbunatatim metodele de lucru. De asemenea, avem un contract colectiv si acordam o prioritate buna bunastarii angajatilor nostri.</p>
+          </div>
+          <div className={classes.infoDiv}>
+            <h3>PARTENER PROFESIONAL</h3>
+            <p>Peste 700 de clienti ne-au ales ca partener obisnuit in curatenie. Stim ca clientii nostri sunt diferiti si,
+              prin urmare, ne concentram puternic pe nevoile si dorintele lor.</p>
+          </div>
+          <div className={classes.infoDiv}>
+            <h3>CONSTIENTI DE MEDIU</h3>
+            <p>Ne-am propus sa protejam mediul. Facem asta, printre altele prin utilizarea agentilor de curatare ecologici.
+              In plus, toti angajatii nostri sunt instruiti in dozarea corecta a agentilor, precum si in metodele de protectie a mediului.
+              Mijloacele noastre de transport sunt, de asemenea, ecologice.</p>
+          </div>
         </div>
-        <div className={classes.SatisfiedCustomersData}>
-          <h1>+3.000</h1>
-          <h1>Curatari lunare</h1>
-        </div>
-      </div>
-      <div className={classes.Details}>
-        <h1>
-          Servicii ecologice si <span>fara</span> perioada minima contractuala
-        </h1>
-        <p>
-          Sunteti pe maini complet sigure daca alegeti CompletCurat pentru a va
-          mentine casa curata si prezentabila. Avem o vasta experienta cu
-          ajutorul curateniei in case private si avem in prezent peste 1.000 de
-          clienti fideli. Stim ca mutarea in casele private este o munca foarte
-          responsabila, motiv pentru care nu facem niciodata compromisuri cu
-          privire la valorile noastre fundamentale. Va tratam bunurile ca si cum
-          ar fi ale noastre si puteti fi siguri ca suntem intotdeauna atenti
-          cand ocolim toate colturile si colturile. Suntem ajutorul tau casnic.
-        </p>
-        <p>
-          Te vei intalni cu echipa permanenta la prima curatenie privata, unde
-          vei parcurge impreuna casa si programul de curatenie agreat.
-          Amintiti-va ca suntem flexibili cu boala, vacante etc. si, prin
-          urmare, puteti anula sau reprograma serviciul de curatenie in orice
-          moment, in conformitate cu politica noastra de anulare. Recomandam
-          clientilor nostri sa achizitioneze o curatare amanuntita de inceput,
-          in care ne asiguram ca ajungem la capatul sarcinii, astfel incat
-          exista un bun punct de plecare de la care sa incepem.
-        </p>
       </div>
       <div className={classes.Details}>
         <h1>
           <span>Ce spun clientii nostri:</span>
         </h1>
         <Carousel />
+      </div>
+      <div className={classes.Details}>
+        <ContactFrom />
       </div>
     </div>
   );
